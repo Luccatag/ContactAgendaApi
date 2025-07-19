fe97b223b586331cfab0c9d6e80178d2b5212f95
Lucca andrade guedes <lucca.aguedes@gmail.com>
2025-07-19 13:42:01 -0700

    feat: initialize contact-agenda-frontend project with Vite and Vue 3
    - Added package.json with dependencies for PrimeVue and Vite.
    - Created tsconfig.app.json for application-specific TypeScript configuration.
    - Added tsconfig.json to manage project references.
    - Introduced tsconfig.node.json for Node-specific TypeScript settings.

--- obj/Debug/net8.0/ContactAgendaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContactAgendaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactAgendaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dedca821fb3fd7e63ea531c31c87d936f7a274db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fef61b695a587101a5b154b8d89edbf58fd0db")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactAgendaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactAgendaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
