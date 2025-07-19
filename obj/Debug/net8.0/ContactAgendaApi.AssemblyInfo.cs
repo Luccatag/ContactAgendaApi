1433cd2c81f5fe90c0f30fd4241fd348b208ba4f
Lucca andrade guedes <lucca.aguedes@gmail.com>
2025-07-18 18:09:17 -0700

    Add solution file and update assembly information for ContactAgendaApi and DTOs

--- obj/Debug/net8.0/ContactAgendaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContactAgendaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactAgendaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92344337db09b9444c5aed000c1d668a3f8c829d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a303d56677178896d71600b0faef4f86a705f61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactAgendaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactAgendaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
